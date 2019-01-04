93ed483cfbfb104c795dac786eec9d8ef227a024
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-04 16:30:06 -0500

    Executed more functions.

--- index.php
+++ index.php
@@ -10,13 +10,17 @@ include "functions.php";
 </head>
 <body>
 <h1>Pair Program 1</h1>
+<pre>
 <?php
 $numbers = array(7, 9, 8, 9, 8, 8, 6);
 printArray($numbers);
-echo "<br>".largest($numbers);
-echo "<br>".average($numbers);
+echo "<br>Largest number: ".largest($numbers);
+echo "<br>Average number: ".average($numbers);
+echo "<br>No duplicates: ".removeDups($numbers);
+echo "<br>Distibution: ".distribution($numbers);
 
 ?>
+</pre>
 
 </body>
 </html>
