c69ddb11225bb6bf295deac2c41e36368348e861
Amanda Williams <amandacwilliams97@gmail.com>
2019-01-04 13:35:56 -0500

    Wrote a function that prints and given array.

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 </head>
 <body>
 <h1>Pair Program 1</h1>
+<?php
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+function printArray($arrayItems){
+    $arrayItems = implode($arrayItems, ', ');
+    echo "$arrayItems";
+}
+
+printArray($numbers);
+
+?>
 
 </body>
 </html>
